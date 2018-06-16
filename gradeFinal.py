cbed923ed6f821950bcef0ba03b62fabc63f5315
davidm19 <dmalone@chadwickschool.org>
2018-06-16 13:35:41 -0700

    Update gradeFinal.py

--- gradeFinal.py
+++ gradeFinal.py
@@ -1,44 +1,40 @@
-class Grader:
-    """docstring for Grader"""
+def grade():
 
-    @classmethod
-    def grade(self):
-        albumTitle = input("What's the album called? ")
-        albumArtist = input("Who is the album by? ")
+  albumTitle = input("What's the album called? ")
+  albumArtist = input("Who is the album by? ")
 
-        bannedArtistList = ["Tori Amos", "Captain Beefheart and His Magic Band", "Captain Beefheart", "Eatmewhileimhot"]
+  bannedArtistList = ["Tori Amos", "Captain Beefheart and His Magic Band", "Captain Beefheart", "Eatmewhileimhot"]
 
-        for artist in bannedArtistList:
-            if albumArtist == bannedArtistList:
-                print("Fuck off.")
+  for artist in bannedArtistList:
+    if albumArtist == bannedArtistList:
+      print("Fuck off.")
 
-        if albumTitle == "In Rainbows":
-            print("This album is perfect.")
+  try:
+    totalNumber = int(input("How many songs are there? "))
+    favNumber = int(input("How many songs were your favorite? "))
+    goodNumber = int(input("How many songs did you think were good? "))
+    mehNumber = int(input("How many songs did you think were neither good nor bad but 'meh'? "))
+    badNumber = int(input("How many songs did you not like? "))
+  except ValueError:
+    print("SYKE... that's the wrong number")
 
-        totalNumber = int(input("How many songs are there? "))
-        favNumber = int(input("How many songs were your favorite? "))
-        goodNumber = int(input("How many songs did you think were good? "))
-        mehNumber = int(input("How many songs did you think were neither good nor bad but 'meh'? "))
-        badNumber = int(input("How many songs did you not like? "))
+  favs = input("Which songs were your favorites? ")
+  bad = input("Which songs were your least favorites? ")
 
-        favs = input("Which songs were your favorites? ")
-        bad = input("Which songs were your least favorites? ")
+  comments = input("Anything else you wanna say about the album? ")
 
-        comments = input("Anything else you wanna say about the album? ")
+  approxgrade = ""
 
-        approxGrade = ""
+  f1 = favNumber * 7
+  g1 = goodNumber * 6
+  m1 = mehNumber * 3
 
-        f1 = favNumber * 7
-        g1 = goodNumber * 6
-        m1 = mehNumber * 3
+  score = (f1 + g1 + m1 + badNumber) / (totalNumber)
 
-        score = (f1 + g1 + m1 + badNumber) / (totalNumber)
+  approxgrade = score * 10
+  approxgrade = approxgrade / 7
 
-        approxgrade = score * 10
-        approxgrade = approxgrade / 7
+  print( albumArtist + " - " + albumTitle + "; " + "Score: " + str(round(approxgrade, 1)) + "; " + comments + " (Favs: " + favs + "; Least Favs: " + bad + ")")
 
-        print( albumArtist + " - " + albumTitle + "; " + "Score: " + str(round(approxgrade, 1)) + "; " + comments + " (Favs: " + favs + "; Least Favs: " + bad + ")")
-
-
-Grader().grade()
-
+if __name__ == "__main__":
+  grade()
